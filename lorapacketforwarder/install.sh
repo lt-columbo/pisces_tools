2617dc4a976c200edc5ab9687b71ac83731e4dc9
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-05-30 20:05:10 -0400

    Update install.sh

--- lorapacketforwarder/install.sh
+++ lorapacketforwarder/install.sh
@@ -3,7 +3,7 @@ echo "--------------------------------------------------------------------------
 echo " Installing connected validator script.                                         "
 echo "--------------------------------------------------------------------------------"
 cd /home/admin/lora-packet-forwarder-analyzer
-wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/lorapacketforwarder/processlogslora.php -O /home/admin/lora-packet-forwarder-analyzer/connected_validator.php
+wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/lorapacketforwarder/processlogslora.php -O /home/admin/lora-packet-forwarder-analyzer/processlogslora.php
 chmod ugo+x /home/admin/lora-packet-forwarder-analyzer/processlogslora.php
 echo "--------------------------------------------------------------------------------"
 echo " To run: type ./processlogslora.php -f                                          "  
