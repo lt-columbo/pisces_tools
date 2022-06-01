bf15ea58f92b1c91b2b67a42f5f8d0a76196a23c
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-01 18:27:58 -0400

    Update install.sh

--- connected_validator/install.sh
+++ connected_validator/install.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 echo "\n"
-echo "---------------------------------------------------------------------------\n"
-echo " Installing connected validator script.\n"
-echo " If this script stalls after downloading sudo is waiting on password.\n"
-echo " Simply type password in and press enter.\n"
-echo "---------------------------------------------------------------------------\n"
+echo "---------------------------------------------------------------------------"
+echo " Installing connected validator script"
+echo "---------------------------------------------------------------------------"
 wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/connected_validator/connected_validator.php -O /home/admin/connected_validator.php
 chmod ugo+x /home/admin/connected_validator.php
 chown admin:admin /home/admin/connected_validator.php
-echo "----------------------------------------------------------------------------\n"
-echo " To run: type sudo connected_validator_info.php\n"
-echo "----------------------------------------------------------------------------\n"
+echo "---------------------------------------------------------------------------"
+echo " To run: type sudo connected_validator_info.php"
+echo "----------------------------------------------------------------------------"
