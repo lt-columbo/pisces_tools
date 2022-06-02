59e928fd760c1aa9a0b99ee00b6974eee0d1630b
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-02 17:10:06 -0400

    Update install.sh

--- lorapacketforwarder/install.sh
+++ lorapacketforwarder/install.sh
@@ -7,12 +7,10 @@ then
 cd /home/admin/lora-packet-forwarder-analyzer
 wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/lorapacketforwarder/processlogslora.php -O /home/admin/lora-packet-forwarder-analyzer/processlogslora.php
 chmod ugo+x /home/admin/lora-packet-forwarder-analyzer/processlogslora.php
-echo "--------------------------------------------------------------------------"
 echo " app installed in: /home/admin/lora-packet-forwarder-analyzer/            "
 echo " To run: type ./processlogslora.php -f                                    "  
 echo "--------------------------------------------------------------------------"
 else 
-echo "--------------------------------------------------------------------------"
 echo " FAILURE. The app failed to install because:                      "
 echo " You must first install the inigoflores lora-packet-analyzer      "
 echo " see https://github.com/inigoflores/lora-packet-forwarder-analyzer"
