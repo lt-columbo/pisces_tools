d6d214f3156fa9dd089cc01ce61ba0be66e19eba
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-03 16:47:05 -0400

    Update install.sh

--- connected_validator/install.sh
+++ connected_validator/install.sh
@@ -5,6 +5,7 @@ echo " Installing connected validator script"
 echo "---------------------------------------------------------------------------"
 wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/connected_validator/connected_validator.php -O /home/admin/connected_validator.php
 chmod ugo+x /home/admin/connected_validator.php
+apt install -y curl php-cli php7.3-curl
 chown admin:admin /home/admin/connected_validator.php
 echo "---------------------------------------------------------------------------"
 echo " To run: type ./connected_validator_info.php"
