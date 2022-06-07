3bddfffbeea01f72187c1ad67a994ba571c3a81b
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-07 08:43:41 -0400

    Update install.sh

--- power_monitor/install.sh
+++ power_monitor/install.sh
@@ -3,7 +3,7 @@ echo "--------------------------------------------------------------------------
 echo " Installing power_monitor.sh script                                   "
 echo "--------------------------------------------------------------------------"
 cd /home/admin/
-wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/power_monitor/power_monitor.sh-O /home/admin/power_monitor.sh
+wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/power_monitor/power_monitor.sh -O /home/admin/power_monitor.sh
 chmod ugo+x /home/admin/power_monitor.sh
 echo " app installed in: /home/admin/           "
 echo " To run: type sudo ./power_monitor.php -f                                    "  
