02de120fb01106a89d8a9cbf4d64dd06d6a93538
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-07 08:28:01 -0400

    Update install.sh

--- power_monitor/install.sh
+++ power_monitor/install.sh
@@ -2,6 +2,7 @@
 echo "--------------------------------------------------------------------------"
 echo " Installing power_monitor.sh script                                   "
 echo "--------------------------------------------------------------------------"
+cd /home/admin/
 wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/power_monitor/power_monitor.sh-O /home/admin/power_monitor.sh
 chmod ugo+x /home/admin/power_monitor.sh
 echo " app installed in: /home/admin/           "
