88f5018a12bcb8f32a624c601b93a20c2a586eef
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-07 08:38:02 -0400

    Delete update_to_602 directory

--- update_to_602/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo docker stop miner
-sudo docker rm miner
-sudo docker run -d --init --ulimit nofile=64000:64000 --restart always --net host -e OTP_VERSION=23.3.4.7 -e REBAR3_VERSION=3.16.1 --name miner --mount type=bind,source=/home/pi/hnt/miner,target=/var/data --mount type=bind,source=/home/pi/hnt/miner/log,target=/var/log/miner --device /dev/i2c-0  --privileged -v /var/run/dbus:/var/run/dbus --mount type=bind,source=/home/pi/hnt/miner/configs/sys.config,target=/config/sys.config quay.io/team-helium/miner:miner-arm64_2022.06.02.1_GA
