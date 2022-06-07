b76eeec36ad30da74e6413849820d26f152d9388
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-07 08:14:54 -0400

    Create install.sh

---
+++ power_monitor/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "--------------------------------------------------------------------------"
+echo " Installing connected validator script.                                   "
+echo "--------------------------------------------------------------------------"
+wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/power_monitor/power_monitor.sh-O /home/admin/power_monitor.sh
+chmod ugo+x /home/admin/power_monitor.sh
+echo " app installed in: /home/admin/           "
+echo " To run: type sudo ./power_monitor.php -f                                    "  
+echo "--------------------------------------------------------------------------"
+fi
