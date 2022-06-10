558e78daa116b841b87238a6747cbfa3aafe4c1c
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-10 17:58:48 -0400

    Delete just-run.sh

--- refresh_dashboard/just-run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/refresh_dashboard/refresh-dash.sh -O - | sudo bash 
-echo " Ran the refresh_dash.sh script from cloud                               "  
-echo "--------------------------------------------------------------------------"
