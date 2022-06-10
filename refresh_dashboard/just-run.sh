c0591919929c2412b2cbf6531dd5561edb9d60d6
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-10 16:59:08 -0400

    Create just-run.sh

---
+++ refresh_dashboard/just-run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/refresh_dashboard/refresh_dash.sh -O - | sudo bash 
+echo " app installed in: /home/admin/           "
+echo " To Ran the refresh_dash.sh script from cloud                             "  
+echo "--------------------------------------------------------------------------"
