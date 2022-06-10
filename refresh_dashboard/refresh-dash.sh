290124da633ff98d3e9082313b01e6006ad1baac
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-10 17:29:43 -0400

    Update refresh-dash.sh

--- refresh_dashboard/refresh-dash.sh
+++ refresh_dashboard/refresh-dash.sh
@@ -5,22 +5,22 @@
 # pubkey used by helium-statuses.sh so do before it
 # ---
 # Animal name
-/etc/monitor_scripts/pubkeys.sh
+/etc/monitor-scripts/pubkeys.sh
 #External Facing IP Address
-/etc/monitor_scripts/external-ip.sh
+/etc/monitor-scripts/external-ip.sh
 # Blockchain Height, Miner Height, Miner Status, lat, long, recent activity
-/etc/monitor_scripts/helium-statuses.sh 
+/etc/monitor-scripts/helium-statuses.sh 
 # info_height run by helium-statuses.sh (local height)
-# /etc/monitor_scripts/info-height.sh
+# /etc/monitor-scripts/info-height.sh
 # Internal IP Address
-/etc/monitor_scripts/local-ip.sh
+/etc/monitor-scripts/local-ip.sh
 # Miner Status
-/etc/monitor_scripts/miner.sh
+/etc/monitor-scripts/miner.sh
 # miner version and latest version
-/etc/monitor_scripts/miner-version-check.sh
+/etc/monitor-scripts/miner-version-check.sh
 # Peer list at bottom of home page
-/etc/monitor_scripts/peer-list.sh
+/etc/monitor-scripts/peer-list.sh
 # Serial Number
-/etc/monitor_scripts/sn-check.sh
+/etc/monitor-scripts/sn-check.sh
 # CPU Temp
-/etc/monitor_scripts/temp.sh
+/etc/monitor-scripts/temp.sh
