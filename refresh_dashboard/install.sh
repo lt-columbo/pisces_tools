bc3552e39c1c468d641a31e90ccb3433b2a5134b
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-10 17:04:57 -0400

    Update install.sh

--- refresh_dashboard/install.sh
+++ refresh_dashboard/install.sh
@@ -3,8 +3,7 @@ echo "--------------------------------------------------------------------------
 echo " Installing refresh_dash.sh script                                   "
 echo "--------------------------------------------------------------------------"
 cd /home/admin/
-wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/refresh_dashboard/refresh_dash.sh -O /home/admin/refresh-dash.sh
+wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/refresh_dashboard/refresh-dash.sh -O /home/admin/refresh-dash.sh
 chmod ugo+x /home/admin/refresh_dash.sh
 echo " app installed in: /home/admin/           "
-echo " To run: type sudo ./refresh_dash.sh                                      "  
-echo "--------------------------------------------------------------------------"
+echo " To run: type sudo ./refresh-dash.sh                                      "  
