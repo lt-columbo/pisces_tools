c07f466a68b0230f26d6feb0bf857241b24bb372
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-13 21:22:56 -0400

    Update refresh-dash.sh
    Send output from fetching external ip and  api uri to bitbucket
    Add output of steps taken and completion message

--- refresh_dashboard/refresh-dash.sh
+++ refresh_dashboard/refresh-dash.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 # Requires sudo
-# Refresh all cached values in used by by dashboard
+# Refresh all cached values in use by dashboard
 # ---
 # pubkey used by helium-statuses.sh so do before it
 # ---
 # Animal name
+echo "Refreshing Pub keys, animal name"
 /etc/monitor-scripts/pubkeys.sh
 #External Facing IP Address
-/etc/monitor-scripts/external-ip.sh
+echo "External IP"
+/etc/monitor-scripts/external-ip.sh  >/dev/null 2>/dev/null
 # Blockchain Height, Miner Height, Miner Status, lat, long, recent activity
-/etc/monitor-scripts/helium-statuses.sh
+echo "Blockchain height, Miner height ..."
+/etc/monitor-scripts/helium-statuses.sh  >/dev/null 2>/dev/null
 # info_height run by helium-statuses.sh (local height)
 # /etc/monitor-scripts/info-height.sh
 # Internal IP Address
+echo "Internal IP Address"
 /etc/monitor-scripts/local-ip.sh
 # Miner Status
+echo "Miner Status"
 /etc/monitor-scripts/miner.sh
 # miner version and latest version
+echo "Miner version, latest version available"
 /etc/monitor-scripts/miner-version-check.sh
 # Peer list at bottom of home page
+echo "Peer list"
 /etc/monitor-scripts/peer-list.sh
 # Serial Number
+echo "Serial Number"
 /etc/monitor-scripts/sn-check.sh
 # CPU Temp
+echo "CPU Temp"
 /etc/monitor-scripts/temp.sh
-echo "Dashboard values refreshed"
+echo "Done. Dashboard values refreshed"
