aa44cbf3e2c598d236d4e360e558a62828c305c0
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-19 11:40:04 -0400

    Update install.sh

--- cpu_temp_monitor/install.sh
+++ cpu_temp_monitor/install.sh
@@ -7,11 +7,14 @@ cd /home/admin/
 wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/cpu_temp_monitor/log_cpu_temp.php -O /home/admin/log_cpu_temp.php
 chmod ugo+x /home/admin/log_cpu_temp.php
 # install phplot in new directory in /var/dashboard
+# create includes if not there
 if [! -d "/var/dashboard/public/includes" ] 
 then
  mkdir /var/dashboard/public/includes
 fi
 wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/cpu_temp_monitor/phplot.php -O /var/dashboard/public/includes/phplot.php
+# install graph_cpu_temp.php in public
+wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/cpu_temp_monitor/graph_cpu_temp.php -O /var/dashboard/public/graph_cpu_temp.php
 # install cpu_temp.php page in dashboard at /var/dashboard/pages/cpu_temp.php
 wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/cpu_temp_monitor/cpu_temp.php -O /var/dashboard/pages/cpu_temp.php
 # install cron job to run logger every 15 minutes
