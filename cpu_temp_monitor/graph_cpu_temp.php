2bc4f8920a505e45b2da2e22ab70a16af3602e0c
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-19 19:42:33 -0400

    Update graph_cpu_temp.php

--- cpu_temp_monitor/graph_cpu_temp.php
+++ cpu_temp_monitor/graph_cpu_temp.php
@@ -2,10 +2,17 @@
 <html>
 <head>
 <title>Pisces Miner CPU Temp Graph</title>
+<link rel="stylesheet" href="css/reset.css" />
+<link rel="stylesheet" href="css/common.css" />
+<link rel="stylesheet" href="css/fonts.css" />
+<link rel="stylesheet" href="css/hack.css" />
 </head>
+
 <body>
+<div id="main">
 <?php
 include('/var/dashboard/pages/cpu_temp.php');
 ?>
+</div>
 </body>
 </html>
