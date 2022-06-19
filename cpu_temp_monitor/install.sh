2ce37db41de98db4620eae611ae9ac5415b6a55e
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-19 10:23:55 -0400

    Update install.sh

--- cpu_temp_monitor/install.sh
+++ cpu_temp_monitor/install.sh
@@ -4,16 +4,16 @@ echo " Installing log_cpu_temp.php script                                       
 echo "--------------------------------------------------------------------------"
 apt -y install php7.3-gd
 cd /home/admin/
-wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/log_cpu_temp/log-cpu-temp.php -O /home/admin/log-cpu-temp.php
-chmod ugo+x /home/admin/log-cpu-temp.php
+wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/cpu_temp_monitor/log_cpu_temp.php -O /home/admin/log_cpu_temp.php
+chmod ugo+x /home/admin/log_cpu_temp.php
 # install phplot in new directory in /var/dashboard
 if [! -d "/var/dashboard/public/includes" ] 
 then
  mkdir /var/dashboard/public/includes
 fi
-wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/log_cpu_temp/phplot.php -O /var/dashboard/public/includes/phplot.php
+wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/cpu_temp_monitor/phplot.php -O /var/dashboard/public/includes/phplot.php
 # install cpu_temp.php page in dashboard at /var/dashboard/pages/cpu_temp.php
-wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/log_cpu_temp/cpu_temp.php -O /var/dashboard/pages/cpu_temp.php
+wget https://raw.githubusercontent.com/lt-columbo/pisces_tools/main/cpu_temp_monitor/cpu_temp.php -O /var/dashboard/pages/cpu_temp.php
 # install cron job to run logger every 15 minutes
 echo "*/15 * * * * /home/admin/log_cpu_temp.php"  >> /var/spool/cron/crontabs/root
 systemctl restart cron.service
