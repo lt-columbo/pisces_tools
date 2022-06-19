8cb51a2941bf9b94add2b042e4691088b84063cd
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2022-06-19 10:59:01 -0400

    Update cpu_temp.php

--- cpu_temp_monitor/cpu_temp.php
+++ cpu_temp_monitor/cpu_temp.php
@@ -140,5 +140,5 @@ $plot->SetLightGridColor('white');
 $plot->SetPrintImage(False);
 $plot->DrawGraph();
 $cpuTempGraph = $plot->EncodeImage();
-$graphHtmlImage = '<img src=" . $cpuTempGraph . '">';
+$graphHtmlImage = '<img src="' . $cpuTempGraph . '">';
 echo $graphHtmlImage;
