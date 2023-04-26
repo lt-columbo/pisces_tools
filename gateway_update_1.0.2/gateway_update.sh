38c5313b5be2d4a1ac18a8bd75c6f688b8636b94
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-04-25 20:31:32 -0400

    Create gateway_update.sh
    Initial commit

---
+++ gateway_update_1.0.2/gateway_update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+old_version=$(/etc/helium_gateway/helium_gateway --version)
+echo "Helium Gateway version installed now is $old_version"
+
+FIRMWARE_VERSION="0.60"
+GATEWAY_RS_PATH="/etc/helium_gateway"
+GATEWAY_VERSION="v1.0.2"
+GATEWAY_FILE="helium-gateway-1.0.2-armv7-unknown-linux-musleabihf.tar.gz"
+
+echo "updating to $GATEWAY_VERSION"
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+# Download the gateway_rs program into the GATEWAY_RS_PATH
+wget "https://github.com/helium/gateway-rs/releases/download/$GATEWAY_VERSION/GATEWAY_FILE" -P "$GATEWAY_RS_PATH/"
+
+# Unzip the gz file into the GATEWAY_RS_PATH
+tar -xvf "$GATEWAY_RS_PATH/$GATEWAY_FILE" -C "$GATEWAY_RS_PATH/"
+
+# Delete the gz file 
+rm "$GATEWAY_RS_PATH/$GATEWAY_FILE"
+
+# Stop the service of helium
+service helium stop 
+
+# Start up the service
+service helium start
+
+echo "Helium_gateway running and updated"
+version=$(/etc/helium_gateway/helium_gateway --version)
+echo "Helium Gateway version now on system is $version"
+echo "Check below to make sure is running"
+systemctl status helium
