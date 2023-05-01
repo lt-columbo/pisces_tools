912d34a095dcb1e297459fd081143a103fa5539c
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-01 12:27:27 -0400

    Update pubkeys.sh

--- dashboard_patch/pubkeys.sh
+++ dashboard_patch/pubkeys.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Updated for Solana helium_gateway 1.0.0
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root - use sudo in front i.e. sudo ${0}" 
+   exit 1
+fi
+
 source /etc/monitor-scripts/dashboard.ini
 
 name_pat='\"name\":\ \"([a-z]*-[a-z]*-[a-z]*)\"'
