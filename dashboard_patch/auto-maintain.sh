9df2d178e1e38af3ef06b3326d198d577e07bbeb
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-01 14:11:05 -0400

    Create auto-maintain.sh

---
+++ dashboard_patch/auto-maintain.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Updated for Solana helium_gateway 1.0.0
+
+source /etc/monitor-scripts/dashboard.ini
+
+service=$(cat ${CFG_FN_AUTO_MAINTAIN} | tr -d '\n')
+
+if [[ $service == 'enabled' ]]; then
+  bash /etc/monitor-scripts/update-check.sh &> /dev/null
+  bash /etc/monitor-scripts/miner-version-check.sh &> /dev/null
+  bash /etc/monitor-scripts/helium-statuses.sh &> /dev/null
+  online_status=$(cat ${CFG_FN_ONLINE_STATUS})
+
+  if [[ ! $online_status =~ 'active' ]]; then
+    echo "[$(date)] Problems with ${CFG_FN_HELIUM_GATEWAY}..." >> ${CFG_FN_AUTO_MAINTAIN_LOG}
+    systemctl restart "${CFG_FN_HELIUM_GATEWAY}"
+    sleep 1m
+    bash /etc/monitor-scripts/helium-statuses.sh &> /dev/null
+    online_status=$(cat ${CFG_FN_ONLINE_STATUS})
+  fi
+
+  if [[ ! $pubkey ]]; then
+    echo "[$(date)] Your public key is missing, trying a refresh..." >> ${CFG_FN_AUTO_MAINTAIN_LOG}
+    bash /etc/monitor-scripts/pubkeys.sh
+  fi
+  
+fi
