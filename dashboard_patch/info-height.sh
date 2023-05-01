8a77021b570982b7836fa5de9b2d60047896880c
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-01 12:26:00 -0400

    Update info-height.sh

--- dashboard_patch/info-height.sh
+++ dashboard_patch/info-height.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # Updated for Solana helium_gateway 1.0.0
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root - use sudo in front i.e. sudo ${0}" 
+   exit 1
+fi
 source /etc/monitor-scripts/dashboard.ini
 echo ${CFG_INFO_HEIGHT} > ${CFG_FN_INFO_HEIGHT}
