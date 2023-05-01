e61eee21b5d43bda13dad2348199d84f4f7e92d5
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-01 09:48:36 -0400

    Update miner-update.sh

--- dashboard_patch/miner-update.sh
+++ dashboard_patch/miner-update.sh
@@ -34,7 +34,7 @@ if [[ ${service} == 'start' ]]; then
 
       echo $(date -u) "Checking if need to remove docker miner"  >> ${CFG_FN_MINER_UPDATE_LOG}
       docker = $(docker ps --format "{{.Image}}" --filter "name=miner" | grep -Po "miner-arm64")
-      if [ "${docker}" -*eq "miner-arm64"} ]; then
+      if [ "${docker}" -eq "miner-arm64"} ]; then
         docker stop miner  
         docker rm miner
         echo $(date -u) "Removed docker miner"  >> ${CFG_FN_MINER_UPDATE_LOG}
