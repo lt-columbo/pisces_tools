495f809b02ea9820fa64324c2897fadd5cabb095
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-01 12:27:11 -0400

    Update peer-list.sh

--- dashboard_patch/peer-list.sh
+++ dashboard_patch/peer-list.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root - use sudo in front i.e. sudo ${0}" 
+   exit 1
+fi
 source /etc/monitor-scripts/dashboard.ini
 echo ${CFG_PEER_LIST_VALUE} > /var/dashboard/statuses/peerlist
