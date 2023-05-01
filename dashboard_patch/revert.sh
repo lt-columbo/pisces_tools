ed9f8e21d3062bb8d489d831bd62e8e97cab6381
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-01 19:09:21 -0400

    Create revert.sh

---
+++ dashboard_patch/revert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root - use sudo in front i.e. sudo ${0}" 
+   exit 1
+fi
+cp /etc/monitor-scripts/auto-maintain.sh.old /etc/monitor-scripts/auto-maintain.sh
+cp /etc/monitor-scripts/helium-statuses.sh.old /etc/monitor-scripts/helium-statuses.sh
+cp /etc/monitor-scripts/info-height.sh.old /etc/monitor-scripts/info-height.sh
+cp /etc/monitor-scripts/miner-update.sh.old /etc/monitor-scripts/miner-update.sh
+cp /etc/monitor-scripts/miner-version-check.sh.old /etc/monitor-scripts/miner-version-check.sh
+cp /etc/monitor-scripts/peer-list.sh.old /etc/monitor-scripts/peer-list.sh
+cp /etc/monitor-scripts/pubkeys.sh.old /etc/monitor-scripts/pubkeys.sh
