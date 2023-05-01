ec388192252eeac50a40daa55ec8b5e77f272576
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-01 12:23:58 -0400

    Update helium-statuses.sh

--- dashboard_patch/helium-statuses.sh
+++ dashboard_patch/helium-statuses.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Updated for Solana helium_gateway 1.0.0
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root - use sudo in front i.e. sudo ${0##*/}" 
+   exit 1
+fi
+
 source /etc/monitor-scripts/dashboard.ini
 
 status_pat='\bActiveState=([A-Za-z]*)\b'
