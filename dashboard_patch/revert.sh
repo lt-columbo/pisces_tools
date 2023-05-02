a79de856cc98b040828af5d47de6e3ae44d34d37
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-01 20:58:34 -0400

    Update revert.sh

--- dashboard_patch/revert.sh
+++ dashboard_patch/revert.sh
@@ -6,6 +6,7 @@ fi
 cp /etc/monitor-scripts/auto-maintain.sh.old /etc/monitor-scripts/auto-maintain.sh
 cp /etc/monitor-scripts/helium-statuses.sh.old /etc/monitor-scripts/helium-statuses.sh
 cp /etc/monitor-scripts/info-height.sh.old /etc/monitor-scripts/info-height.sh
+cp /etc/monitor-scripts/miner.sh.old /etc/monitor-scripts/miner.sh
 cp /etc/monitor-scripts/miner-update.sh.old /etc/monitor-scripts/miner-update.sh
 cp /etc/monitor-scripts/miner-version-check.sh.old /etc/monitor-scripts/miner-version-check.sh
 cp /etc/monitor-scripts/peer-list.sh.old /etc/monitor-scripts/peer-list.sh
