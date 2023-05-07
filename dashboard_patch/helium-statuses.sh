5a3278264d1aab648805335c2e2f33163a540388
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-07 09:38:28 -0400

    Update helium-statuses.sh

--- dashboard_patch/helium-statuses.sh
+++ dashboard_patch/helium-statuses.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Updated for Solana helium_gateway 1.0.0
 
-if [[ $EUID -ne 0 ]]; then
+if [ "$EUID" -ne 0 ]; then
    echo "This script must be run as root - use sudo in front i.e. sudo ${0}" 
    exit 1
 fi
