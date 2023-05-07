af18476fbe1ff023b238eb84eebb848b9c10e16a
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-07 09:52:42 -0400

    Update helium-statuses.sh

--- dashboard_patch/helium-statuses.sh
+++ dashboard_patch/helium-statuses.sh
@@ -13,25 +13,25 @@ status_cmd='systemctl show helium'
 pubkey=$(</var/dashboard/statuses/pubkey)
 root_uri='https://api.helium.io/v1/hotspots/'
 activity_uri="/activity"
-uri="$root_uri$pubkey"
-recent_activity_uri="$uri$activity_uri"
+uri="${root_uri}${pubkey}"
+recent_activity_uri="${uri}${activity_uri}"
 
-data=$(wget -qO- $uri)
-recent_activity=$(curl -s $recent_activity_uri)
+data=$(wget -qO- "$uri")
+recent_activity=$(curl -s "$recent_activity_uri")
 
 height=$(wget -qO- 'https://api.helium.io/v1/blocks/height' | grep -Po '"height":[^}]+' | sed -e 's/^"height"://')
 lat=$(echo $data | grep -Po '"lat":[^\,]+' | sed -e 's/^"lat"://')
 lng=$(echo $data | grep -Po '"lng":[^\,]+' | sed -e 's/^"lng"://')
 
-miner_status=$(systemctl show ${CFG_HELIUM_SERVICE_NAME})
+miner_status=$(systemctl show "$CFG_HELIUM_SERVICE_NAME")
 online_status="none"
-if [[ $miner_status =~ $status_pat ]]; then
+if [[ "$miner_status" =~ $status_pat ]]; then
   online_status="${BASH_REMATCH[1]}"
 fi
 
-echo $online_status > ${CFG_FN_ONLINE_STATUS}
+echo $online_status > "$CFG_FN_ONLINE_STATUS"
 
-echo $lat > ${CFG_FN_LAT}
-echo $lng > ${CFG_FN_LNG}
-echo $height > ${CFG_FN_CUR_BLKHGT}
-echo $recent_activity > ${CFG_FN_RECENT_ACTIVITY}
+echo $lat > "$CFG_FN_LAT"
+echo $lng > "$CFG_FN_LNG"
+echo $height > "$CFG_FN_CUR_BLKHGT"
+echo $recent_activity > "$CFG_FN_RECENT_ACTIVITY"
