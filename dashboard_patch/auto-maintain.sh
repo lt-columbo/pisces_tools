111146e9a08b674917fdc1a21e1666a9f81a5116
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-07 09:37:38 -0400

    Update auto-maintain.sh

--- dashboard_patch/auto-maintain.sh
+++ dashboard_patch/auto-maintain.sh
@@ -5,13 +5,13 @@ source /etc/monitor-scripts/dashboard.ini
 
 service=$(cat ${CFG_FN_AUTO_MAINTAIN} | tr -d '\n')
 
-if [[ $service == 'enabled' ]]; then
+if [ "$service" == 'enabled' ]; then
   bash /etc/monitor-scripts/update-check.sh &> /dev/null
   bash /etc/monitor-scripts/miner-version-check.sh &> /dev/null
   bash /etc/monitor-scripts/helium-statuses.sh &> /dev/null
   online_status=$(cat ${CFG_FN_ONLINE_STATUS})
 
-  if [[ ! $online_status =~ 'active' ]]; then
+  if [[ ! "$online_status" =~ 'active' ]]; then
     echo "[$(date)] Problems with ${CFG_FN_HELIUM_GATEWAY}..." >> ${CFG_FN_AUTO_MAINTAIN_LOG}
     systemctl restart "${CFG_FN_HELIUM_GATEWAY}"
     sleep 1m
@@ -19,7 +19,7 @@ if [[ $service == 'enabled' ]]; then
     online_status=$(cat ${CFG_FN_ONLINE_STATUS})
   fi
 
-  if [[ ! $pubkey ]]; then
+  if [[ ! "$pubkey" ]]; then
     echo "[$(date)] Your public key is missing, trying a refresh..." >> ${CFG_FN_AUTO_MAINTAIN_LOG}
     bash /etc/monitor-scripts/pubkeys.sh
   fi
