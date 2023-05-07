d68c349002e508732affad0e2a734357961aa2df
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-07 09:49:11 -0400

    Update miner-version-check.sh

--- dashboard_patch/miner-version-check.sh
+++ dashboard_patch/miner-version-check.sh
@@ -13,7 +13,7 @@ version_pat='([0-9]+\.[0-9]+\.[0-9]+)'
 
 latest='unknown'
 version_assets=$(curl -s "$CFG_GITHUB_MINER_REPO" | grep -Po "$grep_version_pat")
-if [[ $version_assets =~ "$version_pat" ]]; then
+if [[ $version_assets =~ $version_pat ]]; then
   latest=($(for vers in "${BASH_REMATCH[*]}"; do echo "$vers"; done | sort -unr))
   latest=${latest[0]}
 fi
@@ -23,7 +23,7 @@ echo "$latest" > "$CFG_FN_LATEST_MINER_VER"
 current="unknown"
 helium_cmd="${CFG_DIR_HELIUM_GATEWAY_HOME}${CFG_FN_HELIUM_GATEWAY}"
 helium_current=$("$helium_cmd" --version)
-if [[ "$helium_current" =~ "$version_pat" ]]; then
+if [[ "$helium_current" =~ $version_pat ]]; then
   current="${BASH_REMATCH[1]}"
 fi
 echo $current > $"CFG_FN_CURRENT_MINER_VER"
