95c468e7f4e466a2b16416c69c99bbab1f58f548
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-07 10:00:06 -0400

    Update auto-maintain.sh

--- dashboard_patch/auto-maintain.sh
+++ dashboard_patch/auto-maintain.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # Updated for Solana helium_gateway 1.0.0
 
+if [ "$EUID" -ne 0 ]; then
+   echo "This script must be run as root - use sudo in front i.e. sudo ${0}" 
+   exit 1
+fi
+
 source /etc/monitor-scripts/dashboard.ini
 
-service=$(cat ${CFG_FN_AUTO_MAINTAIN} | tr -d '\n')
+service=$(cat "$CFG_FN_AUTO_MAINTAIN" | tr -d '\n')
 
 if [ "$service" == 'enabled' ]; then
   bash /etc/monitor-scripts/update-check.sh &> /dev/null
@@ -12,15 +17,15 @@ if [ "$service" == 'enabled' ]; then
   online_status=$(cat ${CFG_FN_ONLINE_STATUS})
 
   if [[ ! "$online_status" =~ 'active' ]]; then
-    echo "[$(date)] Problems with ${CFG_FN_HELIUM_GATEWAY}..." >> ${CFG_FN_AUTO_MAINTAIN_LOG}
-    systemctl restart "${CFG_FN_HELIUM_GATEWAY}"
+    echo "[$(date "$CFG_TIME_FORMAT_LOG")] Problems with ${CFG_FN_HELIUM_GATEWAY}..." >> "$CFG_FN_AUTO_MAINTAIN_LOG"
+    systemctl restart "$CFG_FN_HELIUM_GATEWAY"
     sleep 1m
     bash /etc/monitor-scripts/helium-statuses.sh &> /dev/null
-    online_status=$(cat ${CFG_FN_ONLINE_STATUS})
+    online_status=$(cat "$CFG_FN_ONLINE_STATUS")
   fi
 
   if [[ ! "$pubkey" ]]; then
-    echo "[$(date)] Your public key is missing, trying a refresh..." >> ${CFG_FN_AUTO_MAINTAIN_LOG}
+    echo "[$(date "$CFG_TIME_FORMAT_LOG")] Your public key is missing, trying a refresh..." >> "$CFG_FN_AUTO_MAINTAIN_LOG"
     bash /etc/monitor-scripts/pubkeys.sh
   fi
   
