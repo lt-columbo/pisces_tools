1b7bc03401684dd694fd0cacbb9a422efc4657af
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-07 09:53:49 -0400

    Update miner.sh

--- dashboard_patch/miner.sh
+++ dashboard_patch/miner.sh
@@ -1,15 +1,15 @@
 
 #!/bin/bash
 # Updated for Solana helium_gateway 1.0.0
-if [[ $EUID -ne 0 ]]; then
+if [ "$EUID" -ne 0 ]; then
    echo "This script must be run as root - use sudo in front i.e. sudo ${0}" 
    exit 1
 fi
 
 source /etc/monitor-scripts/dashboard.ini
-miner=(cat ${CFG_FN_ONLINE_STATUS})
+miner=(cat "$CFG_FN_ONLINE_STATUS")
 if [ "$miner" == "active" ]; then
-   echo 'true' > ${CFG_FN_MINER}
+   echo 'true' > "$CFG_FN_MINER"
    else
-   echo 'false' > ${CFG_FN_MINER}
+   echo 'false' > "$CFG_FN_MINER"
 fi
