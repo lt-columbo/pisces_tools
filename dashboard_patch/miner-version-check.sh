6d9487fd13244713b6842a41d847adff168555d6
lt-columbo <102260760+lt-columbo@users.noreply.github.com>
2023-05-07 09:43:02 -0400

    Update miner-version-check.sh

--- dashboard_patch/miner-version-check.sh
+++ dashboard_patch/miner-version-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Updated for Solana helium_gateway 1.0.0
 
-if [[ $EUID -ne 0 ]]; then
+if [ "$EUID" -ne 0 ]; then
    echo "This script must be run as root - use sudo in front i.e. sudo ${0}" 
    exit 1
 fi
@@ -12,18 +12,18 @@ grep_version_pat='https://github.com/helium/gateway-rs/releases/expanded_assets/
 version_pat='([0-9]+\.[0-9]+\.[0-9]+)'
 
 latest='unknown'
-version_assets=$(curl -s ${CFG_GITHUB_MINER_REPO} | grep -Po ${grep_version_pat})
-if [[ $version_assets =~ ${version_pat} ]]; then
-  latest=($(for vers in "${BASH_REMATCH[*]}"; do echo "${vers}"; done | sort -unr))
+version_assets=$(curl -s "$CFG_GITHUB_MINER_REPO" | grep -Po "$grep_version_pat")
+if [[ $version_assets =~ "$version_pat" ]]; then
+  latest=($(for vers in "${BASH_REMATCH[*]}"; do echo "$vers"; done | sort -unr))
   latest=${latest[0]}
 fi
 
-echo ${latest} > ${CFG_FN_LATEST_MINER_VER}
+echo "$latest" > "$CFG_FN_LATEST_MINER_VER"
 
 current="unknown"
-helium_cmd=${CFG_DIR_HELIUM_GATEWAY_HOME}${CFG_FN_HELIUM_GATEWAY}
-helium_current=$(${helium_cmd} --version)
-if [[ ${helium_current} =~ ${version_pat} ]]; then
-  current=${BASH_REMATCH[1]}
+helium_cmd="${CFG_DIR_HELIUM_GATEWAY_HOME}${CFG_FN_HELIUM_GATEWAY}"
+helium_current=$("$helium_cmd" --version)
+if [[ "$helium_current" =~ "$version_pat" ]]; then
+  current="${BASH_REMATCH[1]}"
 fi
-echo $current > ${CFG_FN_CURRENT_MINER_VER}
+echo $current > $"CFG_FN_CURRENT_MINER_VER"
